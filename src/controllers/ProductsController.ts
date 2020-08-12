1537394505ce17933e5f33cfb2e1a6ff85451986
joaoptgaino <joaoptgaino@hotmail.com>
2020-08-12 18:28:45 -0300

    Added Create and Read routes

---
+++ src/controllers/ProductsController.ts
@@ -0,0 +1,41 @@
+import {Request,Response} from 'express';
+import db from '../database/connection';
+
+export default class ProductsController{
+    async index(req:Request,res:Response){
+        const products = await db('products')
+        .select()
+        .table('products')
+
+        return res.json(products);
+    }
+    
+    async create(req:Request,res:Response){
+        const{
+            name,
+            price
+        } = req.body;
+        const trx = await db.transaction();
+
+        try{
+            
+            await trx('products').insert(
+                {
+                    name,
+                    price
+                }
+            );
+            await trx.commit();
+            return res.status(201).send();
+        }catch(err){
+            await trx.rollback();
+            return res.status(400).json({
+                error:'Error while creating product'
+            })
+            console.log(err)
+        }
+
+
+    }
+    
+}
